8f92e4ec24bbe18e730bac95426e9f9bd7197f64
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-04 11:36:36 +0700

    Implements the binary search algorithm

--- src/main/java/com/erikriosetiawan/hellospring/HelloSpringApplication.java
+++ src/main/java/com/erikriosetiawan/hellospring/HelloSpringApplication.java
@@ -8,6 +8,10 @@ public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+		int[] numbers = new int[] {3, 5, 9};
+		int findNumber = 9;
+		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		int result = binarySearch.binarySearch(numbers, findNumber);
+		System.out.println(result);
 	}
-
 }
