4eb7ed40f151ee1e5149b6042398a945070dd5cf
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-04 11:18:37 +0700

    Create BinarySearch.java

---
+++ src/main/java/com/erikriosetiawan/hellospring/BinarySearch.java
@@ -0,0 +1,9 @@
+package com.erikriosetiawan.hellospring;
+
+public class BinarySearch implements SortAlgorithm {
+    @Override
+    public int[] sort(int[] numbers) {
+        // Do sorting logic
+        return numbers;
+    }
+}
